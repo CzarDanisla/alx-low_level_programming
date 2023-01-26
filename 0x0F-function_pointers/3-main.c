71a6a10b5a2fec28b8c875488c49e012438ccb59
CzarDanisla <danielasunomeh@gmail.com>
2023-01-25 20:48:18 -0800

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,13 +6,14 @@
 	 * main - Prints the result of simple operations.
 	 * @argc: The number of arguments supplied to the program.
 	 * @argv: An array of pointers to the arguments.
-	 * 
+	 *
 	 * Return: Always 0.
 	 */
 	int main(int __attribute__((__unused__)) argc, char *argv[])
 	{
 		int num1, num2;
 		char *op;
+
 		if (argc != 4)
 		{
 			printf("Error\n");
